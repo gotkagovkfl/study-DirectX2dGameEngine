8338918cdad92f1fd9ca4b6189663776c8b3f41b
김중현 <gotka0211@gmail.com>
2023-01-14 16:02:28 +0900

    (2023.01.14.16:00) 2-2,2-3 key input

--- GameEngine/winmain.cpp
+++ GameEngine/winmain.cpp
@@ -6,13 +6,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
 bool CreateMainWindow(HINSTANCE, int);
 LRESULT WINAPI WinProc(HWND, UINT, WPARAM, LPARAM);
 
-HINSTANCE hinst;
+// 전역변수
+HINSTANCE hinst;	
+HDC hdc;			//	디바이스 컨텍스트의 핸들
+TCHAR ch = ' ';		// 입력한 문자
+RECT rect;			// 사각형 구조체
+PAINTSTRUCT ps;		// WM_PAINT에서 사용함
+bool vkKeys[256];	// 가상 키
 
+// 상수
 const char CLASS_NAME[] = "WinMain";
 const char APP_TITLE[] = "Hello World ang gimochi";
 const int WINDOW_WIDTH = 400;
 const int WINDOW_HEIGHT = 400;
 
+//==========================================
+// 윈도우 애플리케이션의 시작 위치 - main
+//==========================================
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	MSG msg;
@@ -20,8 +30,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// 창을 만든다.
 	if (!CreateMainWindow(hInstance, nCmdShow))
 		return false;
+	
+	// 가상키 배열 초기화 (false로)
+	for (int i = 0; i < 256; i++)  
+		vkKeys[i] = false;
 
-	// main message loop - 끝날때까지 반복
+	// main message loop 
 	int done = 0;
 	while (!done)
 	{
@@ -40,19 +54,138 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 }
 
-
+//==========================================
 // 윈도우 이벤트 콜백함수
-LRESULT WINAPI WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+//==========================================
+LRESULT WINAPI WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	short nVirtKey;                 // virtual-key code 
+	const short SHIFTED = (short)0x8000;
+	TEXTMETRIC tm;                  // structure for text metrics 
+	DWORD chWidth = 20;             // width of characters
+	DWORD chHeight = 20;            // height of characters
+	
+	
 	switch (msg)
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
+		case WM_CREATE:
+			// get the text metrics
+			hdc = GetDC(hwnd);
+			GetTextMetrics(hdc, &tm);
+			ReleaseDC(hwnd, hdc);
+			chWidth = tm.tmAveCharWidth;        // average character width
+			chHeight = tm.tmHeight;             // character height
+			return 0;
+		
+		// 프로그램 종료 요청 시
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+
+		// 키 다운
+		case WM_KEYDOWN:                                // key down
+			vkKeys[wParam] = true;
+			switch (wParam)
+			{
+			case VK_SHIFT:                          // shift key
+				nVirtKey = GetKeyState(VK_LSHIFT);  // get state of left shift
+				if (nVirtKey & SHIFTED)             // if left shift
+					vkKeys[VK_LSHIFT] = true;
+				nVirtKey = GetKeyState(VK_RSHIFT);  // get state of right shift
+				if (nVirtKey & SHIFTED)             // if right shift
+					vkKeys[VK_RSHIFT] = true;
+				break;
+			case VK_CONTROL:                        // control key
+				nVirtKey = GetKeyState(VK_LCONTROL);
+				if (nVirtKey & SHIFTED)             // if left control
+					vkKeys[VK_LCONTROL] = true;
+				nVirtKey = GetKeyState(VK_RCONTROL);
+				if (nVirtKey & SHIFTED)             // if right control
+					vkKeys[VK_RCONTROL] = true;
+				break;
+			}
+			InvalidateRect(hwnd, NULL, TRUE);           // force WM_PAINT
+			return 0;
+			break;
+
+		// 키 업
+		case WM_KEYUP:                                  // key up
+			vkKeys[wParam] = false;
+			switch (wParam)
+			{
+			case VK_SHIFT:                          // shift key
+				nVirtKey = GetKeyState(VK_LSHIFT);
+				if ((nVirtKey & SHIFTED) == 0)      // if left shift
+					vkKeys[VK_LSHIFT] = false;
+				nVirtKey = GetKeyState(VK_RSHIFT);
+				if ((nVirtKey & SHIFTED) == 0)      // if right shift
+					vkKeys[VK_RSHIFT] = false;
+				break;
+			case VK_CONTROL:                        // control key
+				nVirtKey = GetKeyState(VK_LCONTROL);
+				if ((nVirtKey & SHIFTED) == 0)      // if left control
+					vkKeys[VK_LCONTROL] = false;
+				nVirtKey = GetKeyState(VK_RCONTROL);
+				if ((nVirtKey & SHIFTED) == 0)      // if right control
+					vkKeys[VK_RCONTROL] = false;
+				break;
+			}
+			InvalidateRect(hwnd, NULL, TRUE);    // force WM_PAINT
+			return 0;
+			break;
+
+		// 키보드에서 문자 입력시
+		case WM_CHAR:
+			switch (wParam)	// 문자는 wParam에 있음
+			{
+				// 표시되지 못하는 문자의 경우
+				case 0x08:		// 백스페이스
+				case 0x09:		// 탭
+				case 0x0A:		// 라인피드
+				case 0x0D:		// 캐리지 리턴
+				case 0x1B:		// 이스케이프
+					MessageBeep((UINT)-1);	// 비프음 발생
+					return 0;
+				// 표시 가능한 문자의 경우 
+				default:
+					ch = (TCHAR)wParam; 
+					InvalidateRect(hwnd, NULL, TRUE); // WM_PAINT를 강제
+					return 0;
+			}
+		// 윈도우를 다시 그려야하는 경우 
+		case WM_PAINT:
+			hdc = BeginPaint(hwnd, &ps);	// 디바이스 컨텍스트의 핸들을 가져온다. 
+
+			TextOut(hdc, 0, 0, &ch, 1);     // display the character
+
+			// Display the state of vkKeys array
+			// Display 'T' if key is down and 'F' is key is up
+			for (int r = 0; r < 16; r++)
+			{
+				for (int c = 0; c < 16; c++)
+				{
+					if (vkKeys[r * 16 + c])
+					{
+						SetBkMode(hdc, OPAQUE);         // opaque text background
+						TextOut(hdc, c * chWidth + chWidth * 2, r * chHeight + chHeight * 2, "T", 1);
+					}
+					else 
+					{
+						SetBkMode(hdc, TRANSPARENT);    // transparent text background
+						TextOut(hdc, c * chWidth + chWidth * 2, r * chHeight + chHeight * 2, "F", 1);
+					}
+				}
+			}
+			EndPaint(hwnd, &ps);
+			return 0;
 	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
+	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
+
+//==========================================
+// 윈도우 생성 - 에러 발생 시 false 반환
+//==========================================
 bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
 {
 	WNDCLASSEX wcx;
@@ -67,9 +200,9 @@ bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
 	wcx.hInstance = hInstance;
 	wcx.hIcon = NULL;
 	wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcx.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wcx.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
 	wcx.lpszMenuName = NULL;
-	wcx.lpszClassName = (LPCWSTR)CLASS_NAME;
+	wcx.lpszClassName = CLASS_NAME;
 	wcx.hIconSm = NULL;
 	
 	// Window 클래스를 등록한다.
@@ -79,8 +212,8 @@ bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
 
 	// 윈도우 생성
 	hwnd = CreateWindow(
-		(LPCTSTR) CLASS_NAME,
-		(LPCTSTR) APP_TITLE,
+		CLASS_NAME,
+		APP_TITLE,
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
